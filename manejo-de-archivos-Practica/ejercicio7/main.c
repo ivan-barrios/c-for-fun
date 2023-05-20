2c4d681d32a034c395fa5fb8af7147ad25fb1f2e
Ivan Barrios <ivanbar8112003@gmail.com>
2023-05-20 10:41:42 -0300

    Tipos de copia de archivos(falta c)

---
+++ manejo-de-archivos-Practica/ejercicio7/main.c
@@ -0,0 +1,79 @@
+/*
+Escriba un programa que copie el contenido de un archivo de texto en otro nuevo.
+a) Utilizando las funciones fgetc y fputc. <------------------------------------
+b) Utilizando las funciones fgets y fputs.
+c) Utilizando las funciones fread y fwrite
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLEN 500
+
+int main()
+{
+    //VERSION A)
+    /*
+    //Abro y creo archivos a copiar y pegar
+    FILE *arch, *copia;
+    arch = fopen("archivoFuente.txt", "r");
+    copia = fopen("archivoCopia.txt", "w");
+    if ((arch == NULL) || (copia == NULL)) {
+        printf("No se pudo abrir/crear el archivo fuente/copia");
+        return 1;
+    }
+
+    //Variables auxiliares
+    int c;
+
+    //Voy copiando mientras leo
+    c = fgetc(arch);
+    while (!feof(arch)) {
+        fputc(c, copia);
+        c = fgetc(arch);
+    }
+
+    printf("Successful copy");
+
+    //Cierro archivos
+    fclose(arch);
+    fclose(copia);
+
+    return 0;
+    */
+
+    //VERSION B
+    /*
+    FILE *arch, *copia;
+    arch = fopen("archivoFuente.txt", "r");
+    copia = fopen("archivoCopia.txt", "w");
+    if ((arch == NULL) || (copia == NULL)) {
+        printf("No se pudo abrir/crear el archivo fuente/copia");
+        return 1;
+    }
+
+    //Variables auxiliares
+    char line[MAXLEN];
+
+    //Voy copiando mientras leo
+    fgets(line, MAXLEN, arch);
+    while (!feof(arch)) {
+        fputs(line, copia);
+        fgets(line, MAXLEN, arch);
+    }
+
+    printf("Successful copy");
+
+    //Cierro archivos
+    fclose(arch);
+    fclose(copia);
+
+    return 0;
+    */
+
+
+    //VERSION C
+    //???
+
+
+
+}
