4662c7cb8c6426248de371f709c88f222d6aef83
Ivan Barrios <ivanbar8112003@gmail.com>
2023-05-24 12:53:12 -0300

    Ejercicio 9 updated

--- manejo-de-archivos-Practica/ejercicio9/main.c
+++ manejo-de-archivos-Practica/ejercicio9/main.c
@@ -16,8 +16,6 @@ void leerYGuardar(FILE **bf, int cant);
 
 void informarDatos(FILE **f, int cant);
 
-void imprimirDatos(FILE **f, int cant);
-
 
 //En el archivo voy a tener char, unsigned, unsigned, unsigned, int
 int main()
@@ -54,70 +52,37 @@ void leerYGuardar(FILE **bf, int cant){
     for(i=0; i<cant; i++) {
         printf("Nombre: ");
         scanf("%s", j.nombre);
-        fwrite(j.nombre, sizeof(char), 20, *bf);
         printf("Apellido: ");
         scanf("%s", j.apellido);
-        fwrite(j.apellido, sizeof(char), 20, *bf);
         printf("Edad: ");
         scanf("%u", &j.edad);
-        fwrite(&j.edad, sizeof(unsigned int), 1, *bf);
         printf("Cantidad de Titulos: ");
         scanf("%u", &j.cantTitulos);
-        fwrite(&j.cantTitulos, sizeof(unsigned int), 1, *bf);
         printf("Ranking actual: ");
         scanf("%u", &j.ranking);
-        fwrite(&j.ranking, sizeof(unsigned int), 1, *bf);
         printf("Fortuna acumulada: ");
         scanf("%d", &j.fortune);
-        fwrite(&j.fortune, sizeof(int), 1, *bf);
+        fwrite(&j, sizeof(Jugador), 1, *bf);
     }
     printf("Successful reading \n");
 }
 
 
 void informarDatos(FILE **f, int cant) {
-    unsigned int bestRank = 10000, maxTitulos = 0;
-    char rankNom[20], rankAp[20];
-    char titulosNom[20], titulosAp[20];
+    Jugador bestRank;
+    Jugador maxTitulos;
+    bestRank.ranking = 999999;
+    maxTitulos.cantTitulos = 0;
     Jugador j;
     for (int i = 0; i < cant; i++) {
-        fread(j.nombre, sizeof(char), 20, *f);
-        fread(j.apellido, sizeof(char), 20, *f);
-        fread(&j.edad, sizeof(unsigned int), 1, *f);
-        fread(&j.cantTitulos, sizeof(unsigned int), 1, *f);
-        fread(&j.ranking, sizeof(unsigned int), 1, *f);
-        fread(&j.fortune, sizeof(unsigned int), 1, *f);
-        if (bestRank > j.ranking) {
-            bestRank = j.ranking;
-            strcpy(rankNom, j.nombre);
-            strcpy(rankAp, j.apellido);
+        fread(&j, sizeof(Jugador), 1, *f);
+        if (bestRank.ranking > j.ranking) {
+            bestRank = j;
         }
-        if (maxTitulos < j.cantTitulos) {
-            maxTitulos = j.cantTitulos;
-            strcpy(titulosNom, j.nombre);
-            strcpy(titulosAp, j.apellido);
+        if (maxTitulos.cantTitulos < j.cantTitulos) {
+            maxTitulos = j;
         }
     }
-    printf("El jugador con mejor ranking es: %s %s \n", rankNom, rankAp);
-    printf("EL jugador con mas titulos es: %s %s \n", titulosNom, titulosAp);
-}
-
-
-
-void imprimirDatos(FILE **f, int cant) {
-    Jugador j;
-    for (int i = 0; i < cant; i++) {
-        fread(j.nombre, sizeof(char), 20, *f);
-        printf("%s, ", j.nombre);
-        fread(j.apellido, sizeof(char), 20, *f);
-        printf("%s, ", j.apellido);
-        fread(&j.edad, sizeof(unsigned int), 1, *f);
-        printf("%u, ", j.edad);
-        fread(&j.cantTitulos, sizeof(unsigned int), 1, *f);
-        printf("%u, ", j.cantTitulos);
-        fread(&j.ranking, sizeof(unsigned int), 1, *f);
-        printf("%u, ", j.ranking);
-        fread(&j.fortune, sizeof(int), 1, *f);
-        printf("%d\n", j.fortune);
-    }
+    printf("El jugador con mejor ranking es: %s %s \n", bestRank.nombre, bestRank.apellido);
+    printf("EL jugador con mas titulos es: %s %s \n", maxTitulos.nombre, maxTitulos.apellido);
 }
